8a34770e9a7dfc5f02e59b4365fa4f388ed907ef
FreshTaurus <78209340+FreshTaurus@users.noreply.github.com>
2025-01-24 02:32:37 -0500

    Initial commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public Text scoreText;
+    public Text restartText;
+    public Text gameOverText;
+    public Text livesText; // UI element for displaying lives
+
+    private bool gameOver;
+    private bool restart;
+    private int score;
+    private int lives; // Player lives
+
+    void Start()
+    {
+        gameOver = false;
+        restart = false;
+        restartText.text = "";
+        gameOverText.text = "";
+        score = 0;
+        lives = 3; // Initialize lives
+        UpdateScore();
+        UpdateLives(); // Update lives UI
+        StartCoroutine(SpawnWaves());
+    }
+
+    void Update()
+    {
+        if (restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' for Restart";
+                restart = true;
+                break;
+            }
+        }
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void PlayerHit()
+    {
+        lives--; // Decrease lives when the player is hit
+        UpdateLives(); // Update UI
+
+        if (lives <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    void UpdateLives()
+    {
+        livesText.text = "Lives: " + lives;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+
+    // Detect collision with hazards (enemies)
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy")) // Check if the colliding object is an enemy
+        {
+            PlayerHit(); // Reduce lives
+            Destroy(other.gameObject); // Destroy the enemy that hit the player
+        }
+    }
+}
